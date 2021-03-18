eb386681592c53e6b94653ebe2324229592ac41b
sou-pixel <77054594+sou-pixel@users.noreply.github.com>
2021-03-18 14:52:09 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {HttpClientModule} from '@angular/common/http';
+import { CommentComponent } from './comment/comment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent,
+    CommentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
